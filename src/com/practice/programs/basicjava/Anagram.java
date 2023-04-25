8b50134344755fb20720f9ead8ce3f9712167577
Ravindra Farkade <farkaderravindra@gmail.com>
2023-04-25 13:32:37 +0530

    anagram

---
+++ src/com/practice/programs/basicjava/Anagram.java
@@ -0,0 +1,16 @@
+package com.practice.programs.basicjava;
+
+public class Anagram {
+	public static boolean isAnagram(String word1, String word2) {
+if(word1.length()!=word2.length())
+	return false;
+char []
+		return false;
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
